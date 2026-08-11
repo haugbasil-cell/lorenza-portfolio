9d9b447cddb4c329499b92dd32ebf6ee4099c19f
basil haug <basilhaug@MacBook-Pro-von-basil.local>
2026-08-11 21:31:50 +0200

    Lokale Schriftart aktiviert

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,12 @@
-import { Space_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const spaceMono = Space_Mono({
+const myFont = localFont({
+  src: [
+    { path: "./fonts/UN-11STTrial-Regular.woff2", weight: "400", style: "normal" },
+    { path: "./fonts/UN-11STTrial-Italic.woff2", weight: "400", style: "italic" },
+  ],
   variable: "--font-mono",
-  subsets: ["latin"],
-  weight: ["400", "700"],
 });
 
 export const metadata = {
@@ -14,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="de" className={spaceMono.variable}>
+    <html lang="de" className={myFont.variable}>
       <body>{children}</body>
     </html>
   );
