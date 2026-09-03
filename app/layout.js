4d501b841eb1d7d8b50269aca280a053e45a79db
basil haug <basilhaug@MacBook-Pro-von-basil.local>
2026-09-03 12:46:47 +0200

    Switch to final UN-11 font files, bold + uppercase text block

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,28 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const myFont = localFont({
+const unFont = localFont({
   src: [
-    { path: "./fonts/UN-11STTrial-Regular.woff2", weight: "400", style: "normal" },
-    { path: "./fonts/UN-11STTrial-Italic.woff2", weight: "400", style: "italic" },
+    {
+      path: "./fonts/UN11-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/UN11-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/UN11-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/UN11-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
   ],
   variable: "--font-mono",
 });
@@ -14,13 +32,9 @@ export const metadata = {
   description: "Portfolio von Lorenza Longhi",
 };
 
-export const viewport = {
-  themeColor: "#3C3C3C",
-};
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="de" className={myFont.variable}>
+    <html lang="de" className={unFont.variable}>
       <body>{children}</body>
     </html>
   );
